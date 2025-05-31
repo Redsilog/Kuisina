6a8e84daa9cfff21daf4d80570c4f11c7896bc65
Redsilog <Redeligero@gmail.com>
2025-05-31 15:39:54 +0800

    Update playerMovement.cs

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -5,12 +5,18 @@ public class playerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        add();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public int add()
+    {
+        int x = 0;
+        return x + 1;
     }
 }

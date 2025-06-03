8ab7d8e29ad5333c1cd11fe3352117b6593edd12
MrBearA <110599403+MrBearA@users.noreply.github.com>
2025-06-04 01:55:01 +0800

    Reapply "ingredient box/spawn"
    This reverts commit a1fda6557f277697696c442c451259eebb9d9fda.

--- Assets/Script/player/playerMovement.cs
+++ Assets/Script/player/playerMovement.cs
@@ -1,27 +1,52 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class playerMovement : MonoBehaviour
 {
+    [SerializeField] Transform Gameobject;
     public float moveSpeed = 5f;
+    public float mouseSensitivity = 100f;
     public float gravity = -9.81f;
     public float jumpHeight = 1.5f;
 
+    public Transform playerCamera;  // Assign the Camera transform (child of player)
+
     private CharacterController controller;
+    private float xRotation = 0f;
     private Vector3 velocity;
     private bool isGrounded;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        isGrounded = controller.isGrounded;
+        HandleMouseLook();
+        HandleMovement();
+    }
 
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void HandleMovement()
+    {
+        isGrounded = controller.isGrounded;
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f; // small negative value to keep grounded
+            velocity.y = -2f;
         }
 
         float moveX = Input.GetAxis("Horizontal");

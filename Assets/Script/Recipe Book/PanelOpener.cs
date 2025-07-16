7a89207931a2c37d0a295209ee2bd96550271965
ariiezona <115564021+ariiezona@users.noreply.github.com>
2025-07-17 03:38:36 +0800

    recipe book player detection

--- Assets/Script/Recipe Book/PanelOpener.cs
+++ Assets/Script/Recipe Book/PanelOpener.cs
@@ -5,6 +5,7 @@ public class PanelOpener : MonoBehaviour
     [Tooltip("Drag your panel GameObject here in the Inspector.")]
     [SerializeField] private GameObject panel;
     public bool isPanelOpen = false;
+    private bool playerInRange = false;
 
     [Tooltip("Key used to open the panel.")]
     [SerializeField] private KeyCode openKey = KeyCode.E;
@@ -26,12 +27,32 @@ public class PanelOpener : MonoBehaviour
                 panel.SetActive(false);
                 Debug.Log("Closing Recipe Book");
             }
-            else
+            else if(playerInRange)
             {
                 isPanelOpen = true;
                 panel.SetActive(true);
+
                 Debug.Log("Opening Recipe Book");
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("Player in range, can open recipe book");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            isPanelOpen = false;
+            panel.SetActive(false);
+            Debug.Log("Player not in range, cannot open recipe book");
         }
     }
 }

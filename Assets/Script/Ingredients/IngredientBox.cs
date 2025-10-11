387dc0615ac3ee6bd58a2eb133986faff492c57b
Redsilog <Redeligero@gmail.com>
2025-10-11 14:33:44 +0800

    working interact button

--- Assets/Script/Ingredients/IngredientBox.cs
+++ Assets/Script/Ingredients/IngredientBox.cs
@@ -4,45 +4,4 @@ public class IngredientBox : MonoBehaviour
 {
     public string ingredientName;
     public GameObject ingredientPrefab;
-
-    private bool playerInRange = false;
-    private PlayerInventory playerInventory;
-    private KeyCode interactKey = KeyCode.Space;
-
-    void Update()
-    {
-        if (playerInRange && Input.GetKeyDown(interactKey))
-        {
-            if (playerInventory != null && !playerInventory.HasIngredient())
-            {
-                playerInventory.PickUpIngredient(ingredientName, ingredientPrefab);
-                Debug.Log("Dinampot ang " + ingredientName);
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("Player2"))
-        {
-            playerInRange = true;
-            playerInventory = other.GetComponent<PlayerInventory>();
-
-            if (other.CompareTag("Player"))
-                interactKey = KeyCode.Space;
-            else if (other.CompareTag("Player2"))
-                interactKey = KeyCode.Return;
-
-            Debug.Log("Abot: " + other.tag);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("Player2"))
-        {
-            playerInRange = false;
-            playerInventory = null;
-        }
-    }
 }
--- Assets/Script/Main/Players/PlayerController.cs
+++ Assets/Script/Main/Players/PlayerController.cs
@@ -5,16 +5,20 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     [SerializeField] private Transform cameraTransform;
-    private Animator animator;
 
+    private Animator animator;
     private Rigidbody rb;
     private Vector2 moveInput;
+
+    private IngredientBox currentBox;
+    private PlayerInventory inventory;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.linearDamping = 0f;
         animator = GetComponent<Animator>();
+        inventory = GetComponent<PlayerInventory>();
     }
 
     private void FixedUpdate()
@@ -32,7 +36,6 @@ public class PlayerController : MonoBehaviour
         moveDir.Normalize();
 
         rb.linearVelocity = new Vector3(moveDir.x * moveSpeed, rb.linearVelocity.y, moveDir.z * moveSpeed);
-
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDir), 10f * Time.fixedDeltaTime);
         animator.SetBool("IsMoving", true);
     }
@@ -40,5 +43,35 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnInteract(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+        Debug.Log("Interact button works");
+        Debug.Log($"currentBox: {currentBox}, inventory: {inventory}, hasIngredient: {inventory?.HasIngredient()}, hasDish: {inventory?.HasDish()}");
+        if (currentBox != null && inventory != null && !inventory.HasIngredient() && !inventory.HasDish())
+        {
+            inventory.PickUpIngredient(currentBox.ingredientName, currentBox.ingredientPrefab);
+            Debug.Log("Picked up ingredient: " + currentBox.ingredientName);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out IngredientBox box))
+        {
+            currentBox = box;
+            Debug.Log("Box Detected");
+        }   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out IngredientBox box) && box == currentBox)
+        {
+            currentBox = null;
+            Debug.Log("Outside Pickup");
+        }    
     }
 }

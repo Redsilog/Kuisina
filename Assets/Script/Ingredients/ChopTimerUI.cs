532cea630b1fa8e24b79b6c5dbc8de7b330a5167
mnazuki <115564021+mnazuki@users.noreply.github.com>
2025-10-30 16:12:51 +0800

    choptimer ui tweak

--- Assets/Script/Ingredients/ChopTimerUI.cs
+++ Assets/Script/Ingredients/ChopTimerUI.cs
@@ -6,27 +6,12 @@ public class ChopTimerUI : MonoBehaviour
     [Header("References")]
     public ChoppingBoard choppingBoard;
     public Image fillImage;
-    public Vector3 offset = new Vector3(0, 1.5f, 0);
-
-    private Camera mainCam;
-
-    void Start()
-    {
-        mainCam = Camera.main;
-    }
 
     void Update()
     {
-        if (choppingBoard == null || fillImage == null) return;
-
-        transform.position = choppingBoard.transform.position + offset;
-
-        if (mainCam != null)
-            transform.LookAt(mainCam.transform);
-
         fillImage.fillAmount = choppingBoard.ChopProgress01;
 
-        bool show = choppingBoard != null && choppingBoard.IsChoppingActive();
+        bool show = choppingBoard.IsChoppingActive();
         fillImage.enabled = show;
     }
 }

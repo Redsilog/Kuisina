6101164d999523c967a3657ff17773efb1c3e6a8
Redsilog <Redeligero@gmail.com>
2025-11-01 02:40:40 +0800

    Level Timer

--- Assets/Script/Main/Players/LevelManager.cs
+++ Assets/Script/Main/Players/LevelManager.cs
@@ -1,20 +1,55 @@
 using UnityEngine;
+using System.Collections;
 using System;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
 
-    [Header("Stars")]
     public int starsToNextLevel = 5;
     public int currentStars = 0;
 
+    public float levelTime = 0f;
+
     public Action OnStarsReached;
+    public Action OnLevelFailed;
+
+    private float remainingTime;
+    private bool levelActive = false;
 
     private void Awake()
     {
         if (Instance != null && Instance != this) Destroy(gameObject);
         else Instance = this;
+    }
+
+    private void Start()
+    {
+        StartLevel();
+    }
+
+    public void StartLevel()
+    {
+        remainingTime = levelTime;
+        levelActive = true;
+        StartCoroutine(LevelTimer());
+    }
+
+    private IEnumerator LevelTimer()
+    {
+        while (remainingTime > 0 && levelActive)
+        {
+            remainingTime -= Time.deltaTime;
+            Debug.Log($"Time remaining: {remainingTime:F2} seconds");
+            yield return null;
+        }
+
+        if (levelActive && currentStars < starsToNextLevel)
+        {
+            Debug.Log("Time's up! You failed the level.");
+            OnLevelFailed?.Invoke();
+            levelActive = false;
+        }
     }
 
     public void AddStars(int amount)
@@ -29,5 +64,10 @@ public class LevelManager : MonoBehaviour
     public void ResetStars()
     {
         currentStars = 0;
+    }
+
+    public float GetRemainingTime()
+    {
+        return remainingTime;
     }
 }

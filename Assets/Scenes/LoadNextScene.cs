761b5758c10c61c5f56ed9ea1c1a614aecea1a68
Mekiah Gabrielle <134356417+mgbrllc@users.noreply.github.com>
2025-11-25 09:06:46 +0800

    loading screen

--- Assets/Scenes/LoadNextScene.cs
+++ Assets/Scenes/LoadNextScene.cs
@@ -4,7 +4,9 @@ using System.Collections;
 
 public class LoadNextScene : MonoBehaviour
 {
-    public float minDisplayTime = 3f;
+    [SerializeField] private GameObject ls1;
+    [SerializeField] private GameObject ls2;
+    [SerializeField] private GameObject ls3;
 
     void Start()
     {
@@ -13,20 +15,38 @@ public class LoadNextScene : MonoBehaviour
 
     IEnumerator LoadSceneAsync()
     {
+        ls1.SetActive(false);
+        ls2.SetActive(false);
+        ls3.SetActive(false);
+
         float startTime = Time.time;
 
         AsyncOperation operation = SceneManager.LoadSceneAsync(SceneToLoad.nextScene);
         operation.allowSceneActivation = false;
 
+        ls1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        ls1.SetActive(false);
+
+        ls2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        ls2.SetActive(false);
+
+        ls3.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        float elapsed = Time.time - startTime;
+        float minDisplayTime = 4f; 
+        if (elapsed < minDisplayTime)
+        {
+            yield return new WaitForSeconds(minDisplayTime - elapsed);
+        }
+
+        // Activate the scene
+        operation.allowSceneActivation = true;
+
         while (!operation.isDone)
         {
-            float elapsed = Time.time - startTime;
-
-            if (operation.progress >= 0.9f && elapsed >= minDisplayTime)
-            {
-                operation.allowSceneActivation = true;
-            }
-
             yield return null;
         }
     }
